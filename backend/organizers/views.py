5b6e26e51aa0c0572fde781593456cbdc603994d
v15hv4 <vishva2912@gmail.com>
2020-06-03 04:43:47 +0530

    organizers: Re-add authentication for event views

--- backend/organizers/views.py
+++ backend/organizers/views.py
@@ -5,17 +5,21 @@ from base.models import Event
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 
 
+@permission_classes([IsAuthenticated])
 @api_view(["GET"])
 def events(request):
-    events = Event.objects.all()
+    token = request.headers.get("Authorization")[6:]
+    user = Token.objects.get(key=token).user
+    events = Event.objects.filter(user=user)
     serializer = EventSerializer(events, many=True)
     return Response(serializer.data)
 
 
+@permission_classes([IsAuthenticated])
 @api_view(["POST"])
 def events_new(request):
     context = {"request": request}
