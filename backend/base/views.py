fe2d1d6d277fb378fae11de87ae03e3c5f46bbe6
v15hv4 <vishva2912@gmail.com>
2020-06-04 04:30:00 +0530

    base: Send usergroup on token authentication

--- backend/base/views.py
+++ backend/base/views.py
@@ -6,6 +6,9 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def get_token(request):
     user = request.user
+    usergroup = "enduser"
+    if user.groups.exists():
+        usergroup = user.groups.all()[0].name
     token, created = Token.objects.get_or_create(user=user)
-    context = {"token": token.key}
+    context = {"token": token.key, "usergroup": usergroup}
     return render(request, "login_redirect.html", context)
