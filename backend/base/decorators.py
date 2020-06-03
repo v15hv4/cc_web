4c6d35bb65a9b48fbfddd5e25f944323d10f6550
v15hv4 <vishva2912@gmail.com>
2020-06-04 02:05:13 +0530

    base: Introduce usergroup restriction decorator

---
+++ backend/base/decorators.py
@@ -0,0 +1,20 @@
+from django.shortcuts import redirect
+
+from rest_framework.response import Response
+
+
+def allowed_groups(allowed_roles=[]):
+    def decorator(view_func):
+        def wrapper(request, *args, **kwargs):
+            group = None
+            if request.user.groups.exists():
+                print("exists")
+                group = request.user.groups.all()[0].name
+            if group in allowed_roles:
+                return view_func(request, *args, **kwargs)
+            else:
+                return Response("Unauthorized!")
+
+        return wrapper
+
+    return decorator
