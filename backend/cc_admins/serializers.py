ac9b4b1f3cf6008e9aae97a63170423ad877a426
v15hv4 <vishva2912@gmail.com>
2020-07-27 20:27:26 +0530

    backend: Fix auditlog action code for soft deletes

--- backend/cc_admins/serializers.py
+++ backend/cc_admins/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 from auditlog.models import LogEntry
 from base.models import Event, Club, Coordinator
+from json import loads
 
 
 class LogSerializer(serializers.ModelSerializer):
     actor = serializers.ReadOnlyField(source="actor.username")
+    action = serializers.SerializerMethodField()
     event = serializers.SerializerMethodField()
 
     class Meta:
         model = LogEntry
         fields = ["id", "object_pk", "event", "actor", "action", "changes", "timestamp"]
+
+    def get_action(self, obj):
+        changes = loads(obj.changes)
+        if "state" in changes:
+            if changes["state"][1] == "deleted":
+                return 2
+        return obj.action
 
     def get_event(self, obj):
         event = [
