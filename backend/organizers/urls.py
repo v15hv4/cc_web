4be01aefe7283f8370adbfdc4940a40387958158
v15hv4 <vishva2912@gmail.com>
2020-08-14 01:15:49 +0530

    organizers: Add create and read endpoints for budget proposals

--- backend/organizers/urls.py
+++ backend/organizers/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("events/new/", views.events_new),
     path("events/edit/<str:id>/", views.events_edit),
     path("events/delete/<str:id>/", views.events_delete),
+    path("budget/proposals/", views.proposals),
+    path("budget/proposals/new/", views.proposals_new),
 ]
--- backend/organizers/views.py
+++ backend/organizers/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from .serializers import EventSerializer
+from .serializers import EventSerializer, BudgetProposalSerializer
+from .models import BudgetProposal
 from base.models import Event, Club
 from base.decorators import allowed_groups
 from django.utils import timezone
 
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, parser_classes
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.authtoken.models import Token
@@ -53,4 +55,39 @@ def events_delete(request, id):
     event.state = "deleted"
     event.save()
     return Response("Deleted Successfully")
+
+
+@permission_classes([IsAuthenticated])
+@api_view(["GET"])
+@parser_classes([MultiPartParser, FormParser])
+@allowed_groups(allowed_roles=["organizer", "cc_admin"])
+def proposals(request):
+    club = request.query_params.get("club", None)
+    token = request.headers.get("Authorization", None)
+    proposals = BudgetProposal.objects.all()
+
+    # Filter by club
+    if club is not None:
+        proposals = proposals.filter(club=club)
+    if token:
+        mail = Token.objects.get(key=token[6:]).user
+        club = Club.objects.filter(mail=mail).first()
+        if club:
+            proposals = proposals.filter(club=club)
+
+    serializer = BudgetProposalSerializer(proposals, many=True)
+    return Response(serializer.data)
+
+
+@permission_classes([IsAuthenticated])
+@api_view(["POST"])
+@parser_classes([MultiPartParser, FormParser])
+@allowed_groups(allowed_roles=["organizer"])
+def proposals_new(request):
+    serializer = BudgetProposalSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.validated_data["club"] = Club.objects.filter(mail=request.user.username).first()
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors)
 
