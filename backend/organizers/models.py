6e2d8a7fab2023c4ec842f1738c4b2b0ae0c3069
v15hv4 <vishva2912@gmail.com>
2020-09-12 02:45:54 +0530

    organizers: Introduce Update model

--- backend/organizers/models.py
+++ backend/organizers/models.py
@@ -10,3 +10,9 @@ class BudgetProposal(models.Model):
     link = models.TextField(blank=True, null=True)
     pdf = models.FileField(blank=True, null=True)
 
+
+class Update(models.Model):
+    title = models.CharField(max_length=250, blank=False, null=False)
+    content = models.TextField(blank=False, null=False)
+    datetime = models.DateTimeField(default=timezone.now, blank=False, null=False)
+    creator = models.CharField(max_length=250)
