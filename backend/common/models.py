4c2a0888b306ecf13d2edede31b2a58740fd2b91
v15hv4 <vishva2912@gmail.com>
2020-09-23 19:29:24 +0530

    common: Add tag attribute to Update model

--- backend/common/models.py
+++ backend/common/models.py
@@ -1,9 +1,12 @@
 from django.utils import timezone
 from django.db import models
 
+UPDATE_TAG_LIST = [["normal", "NORMAL"], ["reminder", "REMINDER"], ["important", "IMPORTANT"]]
+
 
 class Update(models.Model):
     title = models.CharField(max_length=250, blank=False, null=False)
     content = models.TextField(blank=False, null=False)
     datetime = models.DateTimeField(default=timezone.now, blank=False, null=False)
     creator = models.CharField(max_length=250, blank=False, null=False)
+    tag = models.CharField(max_length=50, choices=UPDATE_TAG_LIST, default="normal")
